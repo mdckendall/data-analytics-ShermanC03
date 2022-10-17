75f4a443be44082510acf9adf2468db07ca21a58
ShermanC03 <111750222+ShermanC03@users.noreply.github.com>
2022-10-16 22:21:49 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,14 +1,52 @@
 import java.io.FileReader;
 import java.util.Scanner;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
 
-		ArrayList<String> arrayList = new ArrayList<>();
-		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
-		
-	}
+    Scanner scanner = new Scanner(System.in);
+    
+    do {
+      System.out.println("Press 1 to learn about salary. "+"\n");
+      System.out.println("Press 2 to learn about the job. "+"\n");
+      System.out.println("Press 3 to learn about demand. "+"\n");
+      System.out.println("Press 4 to view current students. "+"\n");
+      System.out.println("Press 5 to quit. "+"\n");
+      
+      String read = scanner.next();
+      
+      if (read.equals("1")) {
+        System.out.println("$98,345 average salary in South Florida! "+"\n") ;
+         
+      }
+      if (read.equals("2")) {
+        System.out.println("US News - 100 Best Jobs!"+"\n");
+        
+      }
+      if (read.equals("3")) {
+        System.out.println("Top 10 Forbes In-Demand Jobs!"+"\n");
+        
+      }
+      if (read.equals("4")) {
+        System.out.println("Current Students:"+"\n");
+        BufferedReader br = new BufferedReader(new FileReader("names.txt"));
+ 
+    try {
+        String line;
+        while ((line = br.readLine()) != null) {
+           System.out.println(line);
+        }
+    } finally {
+        br.close();
+    }
+      }
+      if (read.equals("5")) {
+        break;
+      } 
+    } 
+      while (true);
+  }
 }
